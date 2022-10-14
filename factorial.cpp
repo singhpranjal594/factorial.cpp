589dac5934e57faf6479b4e927f3729ec4f3afad
singh15varun <61613160+singh15varun@users.noreply.github.com>
2022-10-14 23:25:15 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 #include <bits/stdc++.h>
-int main()
+int main() //main method
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     long long n,i,prod=1;
-    cin >> n;
+    cin >> n;  //taking input
 long long i;
 for(i=1;i<=n;i++)
 prod*=i;     //calculating factoraial 
